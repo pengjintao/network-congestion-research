0b928667766364990b2a3e6dc4386b3aa5b65c33
pengjintao <1272813056@qq.com>
2018-10-11 21:23:37 +0800

    changed

--- latency_bandwith_method.py
+++ latency_bandwith_method.py
@@ -1,9 +1,10 @@
  #!/usr/bin/python
 import model_calc as MC
 import queue
+import copy
 
 def update_start(e,G):
-    print("start update")
+   # print("start update")
     if e.Congestion:
         e.Congestion = False
     else:
@@ -12,14 +13,14 @@ def update_start(e,G):
         P = e.Start
         P.Msgs.ExtractPacket(e,G)
     return e.curPacket
-def update(e,Q,G):
-    print("link update")
+def update(e,Q,G,S,NodeMap):
+    #print("link update")
     if e.Congestion:
         e.Congestion = False
         for l in e.InEdges:
             if l.curPacket.nextlink(G) == e:
                 l.Congestion = True
-                Q.put(l)
+                insert_edge_to_queue(e,Q,S,NodeMap)
         return e.curPacket
     tag = True
     backup = e.roundRobin
@@ -37,44 +38,63 @@ def update(e,Q,G):
                 e.InEdges[index].curPacket.clear()
             else:
                 e.InEdges[index].Congestion = True
-            
-            Q.put(e.InEdges[index])
+            insert_edge_to_queue(e.InEdges[index],Q,S,NodeMap)
     e.roundRobin = backup
     return e.curPacket
-    
+
+def insert_edge_to_queue(e,Q,S,NodeMap):
+    if not e in S:
+        Q.put(e)
+        S.add(e)
+        NodeMap[e.Start] -= 1
+        if NodeMap[e.Start] == 0:
+            for e1 in e.Start.InEdges:
+                insert_edge_to_queue(e1,Q,S,NodeMap)
 def latency_bandwith_estimate(G, MsgD):
     print("start")
     MC.add_msg_to_Node(MsgD,G)
     
+    NodeOutEdgeMap = {}
+    for key,x in G.NodeMap.items():
+        NodeOutEdgeMap[x] = set()
+    for key,data in MsgD.items():
+        for e in G.MsgRout[data["Msg"].Start.Iph_I][data["Msg"].End.Iph_I]:
+            NodeOutEdgeMap[e.Start].add(e)
+    NodeOutDgreeMap = {}
+    for key,data in NodeOutEdgeMap.items():
+        NodeOutDgreeMap[key] = len(data)
     all_time = 0.0
     time_step  = 1.0
     print(G.MsgCounter)
     while not G.MessageEmpty():
-        # print("test")
+        #print(G.MsgCounter)
         #更新一个时间步
         all_time += 1
         Q = queue.Queue()
         S = set()
+        NodeMap = copy.copy(NodeOutDgreeMap)
         #将接受端所有的入边加入队列
         for o in G.OutNode:
             for e in o.InEdges:
-                Q.put(e)
-                S.add(e)
+                insert_edge_to_queue(e,Q,S,NodeMap)
 
         #更新整个网络
         pack = MC.packet()
         while not Q.empty():
+            #print(Q.qsize())
             e = Q.get()
             if e.Start.type == 0:
-                print("Input edge")
+               # print("Input edge")
                 pack = update_start(e,G) 
             else:
-                pack = update(e,Q,G)
-            if pack.Msg != None and e.End.type == 2:
-                e.curPacket.clear()
+                pack = update(e,Q,G,S,NodeMap)
             if pack.Msg != None and e.End.type == 2 and pack.PSN == pack.Msg.size:
                 G.MsgCounter -= 1
+                print("check")
                 MsgD[pack.Msg.Start.label + pack.Msg.End.label]["time"] = all_time
                 print("%s finised"%(pack.Msg.Start.label + pack.Msg.End.label))
-     #   break
+            if pack.Msg != None and e.End.type == 2:
+                e.curPacket.clear()
+                print("check1")
+        
     return all_time
